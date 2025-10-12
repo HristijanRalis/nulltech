b0fd7dec5a87bed5edc45c6b93ffd216df950d0e
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-10-13 00:15:42 +0200

    Added some styles in Contact

--- nulltech/src/pages/Contact/Contact.tsx
+++ nulltech/src/pages/Contact/Contact.tsx
@@ -25,17 +25,19 @@ export const Contact = () => {
           <div className="col-12  col-md-6 col-lg-3 ">
             <h3 className="contactTitle">offices</h3>
             <ul className="contactList p-4">
-              <li className="contactItem d-flex gap-3">
+              <li className="contactItem d-flex align-items-center gap-3">
                 <FontAwesomeIcon icon={faLocationDot} />
-                <p>Skopje, North Macedonia</p>
+                <p className="contactItemDescription">
+                  Skopje, North Macedonia
+                </p>
               </li>
               <li className="contactItem d-flex gap-3">
                 <FontAwesomeIcon icon={faLocationDot} />
-                <p>Ljubljana, Slovenia</p>
+                <p className="contactItemDescription">Ljubljana, Slovenia</p>
               </li>
               <li className="contactItem d-flex gap-3">
                 <FontAwesomeIcon icon={faLocationDot} />
-                <p>Belgrade, Serbia</p>
+                <p className="contactItemDescription">Belgrade, Serbia</p>
               </li>
             </ul>
           </div>
@@ -43,17 +45,19 @@ export const Contact = () => {
           <div className="col-12  col-md-6 col-lg-3">
             <h3 className="contactTitle">e-mail</h3>
             <ul className="contactList p-4">
-              <li className="contactItem d-flex gap-3">
+              <li className="contactItem d-flex align-items-center gap-3">
                 <FontAwesomeIcon icon={faEnvelope} />
-                <p>info@nulltech.ai</p>
+                <p className="contactItemDescription">info@nulltech.ai</p>
               </li>
-              <li className="contactItem d-flex gap-3">
+              <li className="contactItem d-flex  align-items-center gap-3">
                 <FontAwesomeIcon icon={faEnvelope} />
-                <p>vasko.d@nulltech.ai</p>
+                <p className="contactItemDescription">vasko.d@nulltech.ai</p>
               </li>
-              <li className="contactItem d-flex gap-3">
+              <li className="contactItem d-flex  align-items-center gap-3">
                 <FontAwesomeIcon icon={faEnvelope} />
-                <p>aleksandar.d@nulltech.ai</p>
+                <p className="contactItemDescription">
+                  aleksandar.d@nulltech.ai
+                </p>
               </li>
             </ul>
           </div>
@@ -61,17 +65,17 @@ export const Contact = () => {
           <div className="col-12  col-md-6 col-lg-3">
             <h3 className="contactTitle">phone</h3>
             <ul className="contactList p-4">
-              <li className="contactItem d-flex gap-3">
+              <li className="contactItem d-flex  align-items-center gap-3">
                 <FontAwesomeIcon icon={faPhone} />
-                <p>+38978258848</p>
+                <p className="contactItemDescription">+38978258848</p>
               </li>
-              <li className="contactItem d-flex gap-3">
+              <li className="contactItem d-flex  align-items-center gap-3">
                 <FontAwesomeIcon icon={faPhone} />
-                <p>+38970221997</p>
+                <p className="contactItemDescription">+38970221997</p>
               </li>
-              <li className="contactItem d-flex gap-3">
+              <li className="contactItem d-flex align-items-center gap-3">
                 <FontAwesomeIcon icon={faPhone} />
-                <p>+38978313742</p>
+                <p className="contactItemDescription">+38978313742</p>
               </li>
             </ul>
           </div>
@@ -79,13 +83,13 @@ export const Contact = () => {
           <div className="col-12  col-md-6 col-lg-3">
             <h3 className="contactTitle">website</h3>
             <ul className="contactList p-4">
-              <li className="contactItem d-flex gap-3 text-center">
+              <li className="contactItem d-flex align-items-center gap-3 text-center">
                 <FontAwesomeIcon icon={faGlobe} />
-                <p>nulltech.ai</p>
+                <p className="contactItemDescription">nulltech.ai</p>
               </li>
               <li className="contactItem d-flex gap-3">
                 <FontAwesomeIcon icon={faGlobe} />
-                <p>nulltech.ai</p>
+                <p className="contactItemDescription">nulltech.ai</p>
               </li>
             </ul>
           </div>
