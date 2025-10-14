9e8b6ca9e8d38a049cf166be83d60878d4075070
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-10-14 15:55:23 +0200

    Done with Service Page

--- nulltech/src/components/OurServices.tsx
+++ nulltech/src/components/OurServices.tsx
@@ -61,6 +61,70 @@ export const OurServices = () => {
             </span>
           </h4>
         </div>
+
+        <div className="mobileOurServiceContent ">
+          <StackContainer name="OUR SERVICES" />
+
+          <ul className="servicesList">
+            <li className="serviceListItem">
+              <h4 className=" serviceTitle my-5">
+                CLOUD SERVICES <br /> (MIGRATION & MANAGEMENT)
+              </h4>
+            </li>
+            <li className="serviceListItem">
+              <h4 className="serviceTitle">UI/UX DESIGN SERVICES</h4>
+            </li>
+            <li className="serviceListItem">
+              <h4 className="serviceTitle">INTEGRATION DEVELOPMENT</h4>{" "}
+            </li>
+            <li className="serviceListItem">
+              <h4 className="serviceTitle">MOBILE APP DEVELOPMENT </h4>{" "}
+            </li>
+            <li className="serviceListItem">
+              <h4 className="serviceTitle">DIGITAL TRANSFORMATION</h4>
+            </li>
+            <li className="serviceListItem">
+              <h4 className="serviceTitle">
+                DATA ENGINEERING & BI
+                <span className="desc">
+                  Data warehouse setup (Snowflake, BigQuery), ETL pipelines,
+                  <br />
+                  BI dashboards (Power BI, Tableau)
+                </span>
+              </h4>
+            </li>
+            <li className="serviceListItem">
+              <h4 className="serviceTitle">
+                DEVOPS & AUTOMATION
+                <span className="desc">
+                  CI/CD pipeline, infrastructure automation (Terraform,
+                  Ansible),
+                  <br />
+                  Monitoring and alerting (Prometheus, Grafana)
+                </span>
+              </h4>
+            </li>
+
+            <li className="serviceListItem">
+              <h4 className="serviceTitle">
+                AI & DATA SCIENCE SOLUTIONS
+                <span className="desc">
+                  Chatbots, predictive analytics,
+                  <br />
+                  AI-driven automation
+                </span>
+              </h4>
+            </li>
+            <li className="serviceListItem">
+              <h4 className="serviceTitle">
+                MANAGED IT SERVICES (MSP)
+                <span className="desc">
+                  24/7 support, monitoring, patch management
+                </span>
+              </h4>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
